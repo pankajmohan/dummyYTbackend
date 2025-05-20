581a886e4115409232e1d8f86ef8951cb2f5124d
Pankaj Mohan <pankaj.mohan282@gmail.com>
2025-05-20 21:03:34 +0530

    util code discussion

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+dotenv.config({
+    
+})
+
+connectDB()
+.then(()=>{
+    app.on("error", (error)=>{
+        console.log("Err: ", error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port :${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!", err)
+})
+// used ifyy
+// (async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     } catch (error) {
+//         console.log("ERROR:", error);
+//         throw error
+        
+//     }
+// })()
+
+
